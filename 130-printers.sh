93b94b9f9e6c837141f6f7c3b3f22da1900403aa
Erik Dubois <erik.dubois@gmail.com>
2017-10-20 13:38:01 +0200

    printers

---
+++ 130-printers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -S --noconfirm --needed cups cups-pdf 
+
+sudo pacman -S foomatic-db-engine --noconfirm --needed
+sudo pacman -S foomatic-db foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds --noconfirm --needed
+sudo pacman -S ghostscript gsfonts gutenprint --noconfirm --needed
+sudo pacman -S libcups --noconfirm --needed
+sudo pacman -S hplip --noconfirm --needed
+sudo pacman -S system-config-printer --noconfirm --needed
+
+
+sudo systemctl enable org.cups.cupsd.service
