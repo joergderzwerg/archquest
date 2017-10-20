a062352d0b219d6d17099c568e684c39927a83be
Arch MergeD <archmerged@gmail.com>
2017-10-20 14:52:00 +0200

    printers without foomatic

--- 130-printers-v2.sh
+++ 130-printers-v2.sh
@@ -8,8 +8,9 @@ set -e
 
 sudo pacman -S --noconfirm --needed cups cups-pdf 
 
-sudo pacman -S foomatic-db-engine --noconfirm --needed
-sudo pacman -S foomatic-db foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds --noconfirm --needed
+#first try if you can print without footmatic
+#sudo pacman -S foomatic-db-engine --noconfirm --needed
+#sudo pacman -S foomatic-db foomatic-db-ppds foomatic-db-nonfree-ppds foomatic-db-gutenprint-ppds --noconfirm --needed
 sudo pacman -S ghostscript gsfonts gutenprint --noconfirm --needed
 sudo pacman -S libcups --noconfirm --needed
 sudo pacman -S hplip --noconfirm --needed
