46f5ffe4d22edba7b4169e5bba6b05dcba5f0e95
archquest <joergderzwerglange@gmail.com>
2017-12-30 21:43:29 +0100

    added geary as E-Mail-app

--- install-internet-apps.sh
+++ install-internet-apps.sh
@@ -4,6 +4,7 @@ set -e
 sudo pacman -S firefox --noconfirm --needed
 sudo pacman -S firefox-i18n-de --noconfirm --needed
 sudo pacman -S flashplugin --noconfirm --needed
+sudo pacman -S geary --noconfirm --needed
 
 
 
